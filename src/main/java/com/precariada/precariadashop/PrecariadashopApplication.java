b50916669ee7ae4301b919c076940be19d926183
Thais Rocha <thaisrochadequeiroz@gmail.com>
2025-06-26 11:23:16 +0200

    first commit

---
+++ src/main/java/com/precariada/precariadashop/PrecariadashopApplication.java
@@ -0,0 +1,13 @@
+package com.precariada.precariadashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecariadashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecariadashopApplication.class, args);
+	}
+
+}
