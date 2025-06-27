0bdd871f83e0c20bcd563b375e65cf15f19a44ec
Lara Pla <laraplamoreno@gmail.com>
2025-06-27 16:25:01 +0200

    feat(data.sql, application.properties, Product): add data.sql file and @Column annotation

--- src/main/java/com/precariada/precariadashop/models/Product.java
+++ src/main/java/com/precariada/precariadashop/models/Product.java
@@ -10,6 +10,8 @@ public class Product {
     private Long id;
     private String name;
     private double price;
+
+    @Column(name = "image_url")
     private String imageUrl;
     private boolean featured;
     // private Category category;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO products (name, price, image_url, featured) VALUES
+('Lámina "Titis"', 29.99, 'https://imagen.jpg', 1),
+('Camiseta "Envejecer con las amigas"', 49.99, 'https://imagen.jpg', 0);
