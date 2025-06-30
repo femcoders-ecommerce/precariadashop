9cb38bb065719fdfea1b27102ef3412cf44c1fe1
Lara Pla <laraplamoreno@gmail.com>
2025-06-30 15:09:58 +0200

    feat(UserController): add getAllUsers and getUserById methods

---
+++ src/main/java/com/precariada/precariadashop/controllers/UserController.java
@@ -0,0 +1,31 @@
+package com.precariada.precariadashop.controllers;
+
+import com.precariada.precariadashop.dtos.users.UserRequest;
+import com.precariada.precariadashop.dtos.users.UserResponse;
+import com.precariada.precariadashop.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        List<UserResponse> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        UserResponse userResponse = userService.getUserById(id);
+        return ResponseEntity.ok(userResponse);
+    }
+}
