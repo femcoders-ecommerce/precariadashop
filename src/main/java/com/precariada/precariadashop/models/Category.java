7a466b16e8f06b09961bfdc502f3e0c6c8bdb914
may-leth <mayleth9609@gmail.com>
2025-06-30 12:42:09 +0200

    feat(Category): add constructors, getters and setters

---
+++ src/main/java/com/precariada/precariadashop/models/Category.java
@@ -0,0 +1,42 @@
+package com.precariada.precariadashop.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> products = new ArrayList<>();
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
