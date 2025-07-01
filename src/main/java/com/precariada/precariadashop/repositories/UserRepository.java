f7969b27e21c24ff43ab4447a4d7f0a3d0a259e1
Lara Pla <laraplamoreno@gmail.com>
2025-07-01 02:19:19 +0200

    fix(UserRepository): add repository annotation

--- src/main/java/com/precariada/precariadashop/repositories/UserRepository.java
+++ src/main/java/com/precariada/precariadashop/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.precariada.precariadashop.repositories;
 
 import com.precariada.precariadashop.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
