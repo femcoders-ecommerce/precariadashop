a96b418c1f57fa628f2277dea5dae83f9667e9ed
may-leth <mayleth9609@gmail.com>
2025-07-01 16:19:16 +0200

    chore(data.sql): add categories and users data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,11 @@
-INSERT INTO products (name, price, image_url, featured) VALUES
-('Lámina "Titis"', 29.99, 'https://imagen.jpg', 1),
-('Camiseta "Envejecer con las amigas"', 49.99, 'https://imagen.jpg', 0);
+INSERT INTO categories (name) VALUES
+('Textil'),
+('Print');
+
+INSERT INTO products (name, price, image_url, featured, category_id) VALUES
+('Lámina "Titis"', 29.99, 'https://imagen.jpg', 1, 2),
+('Camiseta "Envejecer con las amigas"', 49.99, 'https://imagen.jpg', 0, 1);
+
+INSERT INTO users (username, email, password) VALUES
+('Lara', 'lara@correo.com', 'Lara123.'),
+('Sofía', 'soffinutria@nutria.com', 'Nutria321.');
