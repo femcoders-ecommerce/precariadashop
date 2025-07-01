54fc1d4220a9bf8cd635bb3d37f972939229c531
Lara Pla <laraplamoreno@gmail.com>
2025-07-01 02:26:30 +0200

    fix(User): add entity and id annotations

--- src/main/java/com/precariada/precariadashop/models/User.java
+++ src/main/java/com/precariada/precariadashop/models/User.java
@@ -1,6 +1,12 @@
 package com.precariada.precariadashop.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String email;
